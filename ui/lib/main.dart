4c9a2a832765d674737f2ff66974be3e97acdf0c
Eric Seidel <eric@shorebird.dev>
2024-11-11 14:21:29 -0800

    chore: move ui to old_ui and make new template

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,170 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import 'geometry.dart';
-import 'view.dart';
-import 'world.dart';
 
 void main() {
-  runApp(const GameApp());
+  runApp(const MyApp());
 }
 
-class GameApp extends StatelessWidget {
-  const GameApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Factory',
-      theme: ThemeData.dark(),
-      home: const GamePage(),
-    );
-  }
-}
-
-class ItemSelector extends StatelessWidget {
-  final List<ItemType> items;
-  final ValueChanged<ItemType> onItemSelected;
-  final ItemType selectedItem;
-  final Direction selectedDirection;
-
-  const ItemSelector({
-    super.key,
-    required this.items,
-    required this.onItemSelected,
-    required this.selectedItem,
-    required this.selectedDirection,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        final item = items[index];
-        final cell = PlacedItem(
-          location: Position.zero,
-          type: item,
-          facingDirection: selectedDirection,
-        );
-        var button = Container(
-          color: item.color,
-          child: Center(
-            child: Text(
-              cell.toCharRepresentation(),
-              style: const TextStyle(fontSize: 24),
-            ),
-          ),
-        );
-        if (item == selectedItem) {
-          button = Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white, width: 2),
-            ),
-            child: button,
-          );
-        }
-        return GestureDetector(
-          onTap: () => onItemSelected(item),
-          child: button,
-        );
-      },
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class GamePage extends StatefulWidget {
-  const GamePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  State<GamePage> createState() => _GamePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _GamePageState extends State<GamePage>
-    with TickerProviderStateMixin<GamePage> {
-  final focusNode = FocusNode();
-  late GameController controller;
-  final gameKey = GlobalKey();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  ItemType selectedItem = ItemType.wall;
-  Direction selectedDirection = Direction.up;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = GameController(this);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.blueGrey,
-      child: Center(
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 1.0,
-              child: KeyboardListener(
-                autofocus: true,
-                focusNode: focusNode,
-                onKeyEvent: (event) {
-                  if (event is KeyDownEvent &&
-                      event.logicalKey == LogicalKeyboardKey.keyR) {
-                    setState(() {
-                      selectedDirection = selectedDirection.rotateRight();
-                    });
-                    return;
-                  }
-
-                  if (event is KeyDownEvent || event is KeyRepeatEvent) {
-                    controller.handleKeyEvent(event);
-                  }
-                },
-                child: Listener(
-                  onPointerDown: (event) {
-                    focusNode.requestFocus();
-                    final renderBox =
-                        gameKey.currentContext!.findRenderObject() as RenderBox;
-                    final size = renderBox.size;
-                    final worldPosition =
-                        controller.hitTest(event.localPosition, size);
-                    controller.placeItem(
-                      position: worldPosition,
-                      itemType: selectedItem,
-                      direction: selectedDirection,
-                    );
-                  },
-                  child: GameView(
-                    key: gameKey,
-                    controller: controller,
-                  ),
-                ),
-              ),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            Expanded(
-              child: ItemSelector(
-                items: ItemType.values,
-                onItemSelected: (item) {
-                  setState(() {
-                    selectedItem = item;
-                  });
-                },
-                selectedItem: selectedItem,
-                selectedDirection: selectedDirection,
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
