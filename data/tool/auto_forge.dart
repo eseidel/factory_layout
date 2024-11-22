974e72884bc11b9f8a630adb11ccd18066d0d46d
Eric Seidel <eric@shorebird.dev>
2024-11-21 19:11:49 -0800

    feat: move to a lua parser instead of regexp

--- data/tool/auto_forge.dart
+++ data/tool/auto_forge.dart
@@ -4,6 +4,9 @@
 import 'dart:io';
 
 import 'package:collection/collection.dart';
+import 'package:lua_dardo/src/compiler/ast/exp.dart';
+import 'package:lua_dardo/src/compiler/ast/stat.dart';
+import 'package:lua_dardo/src/compiler/parser/parser.dart';
 
 class Item {
   final String name;
@@ -22,6 +25,14 @@ enum CraftSite {
   forge,
   player,
   warfare,
+  infuser,
+  elementalizer,
+  manufacturer,
+  refiner,
+  compressor,
+  distiller,
+  byproduct,
+  replicator,
   lavaForge;
 
   static CraftSite fromString(String s) {
@@ -75,58 +86,193 @@ class Recipe {
   final int id;
   final bool unlocked;
 
-  Recipe(this.name, this.inputs, this.outputs, this.duration, this.sites,
-      this.tab, this.id, this.unlocked);
+  Recipe({
+    required this.name,
+    required this.inputs,
+    required this.outputs,
+    required this.duration,
+    required this.sites,
+    required this.tab,
+    required this.id,
+    required this.unlocked,
+  });
 }
 
 class RecipeParser {
-  Map<Item, int> itemCountsFromLua(String lua) {
+  Set<CraftSite> _parseCraftSites(Exp exp) {
+    if (exp is TableAccessExp) {
+      final prefix = (exp.prefixExp as NameExp?)?.name;
+      if (prefix != 'CraftSite') {
+        throw 'Invalid CraftSite $exp on line ${exp.lastLine}';
+      }
+      final key = (exp.keyExp as StringExp).str;
+      return {CraftSite.fromString("$prefix.$key")};
+    }
+    if (exp is FuncCallExp) {
+      return exp.args.map(_parseCraftSites).expand((e) => e).toSet();
+    }
+    throw 'Invalid CraftSite $exp on line ${exp.lastLine}';
+  }
+
+  Duration _parseDuration(Exp exp) {
+    if (exp is! FuncCallExp) {
+      throw 'Invalid duration $exp on line ${exp.lastLine}';
+    }
+    final name = (exp.prefixExp as NameExp).name;
+    if (name == 'seconds') {
+      final arg = exp.args.first;
+      final int milliseconds;
+      if (arg is IntegerExp) {
+        milliseconds = arg.val * 1000;
+      } else if (arg is FloatExp) {
+        milliseconds = (arg.val * 1000).toInt();
+      } else {
+        throw 'Invalid duration $exp on line ${exp.lastLine}';
+      }
+      return Duration(milliseconds: milliseconds);
+    } else {
+      throw 'Unsupported duration type $name $exp on line ${exp.lastLine}';
+    }
+  }
+
+  (Item, int) _parseItemCount(Exp exp) {
+    // Can be either:
+    //  Item.create("material.mineral", 5),
+    // __TS__New(FluidItem, FluidType.Water, 10)
+    if (exp is! FuncCallExp) {
+      throw 'Invalid item count $exp on line ${exp.lastLine}';
+    }
+    final String name;
+    final Exp prefixExp = exp.prefixExp;
+    if (prefixExp is NameExp) {
+      name = (prefixExp).name;
+    } else if (prefixExp is TableAccessExp) {
+      final key = prefixExp.keyExp;
+      if (key is StringExp) {
+        name = key.str;
+      } else {
+        throw 'Invalid item count $exp on line ${exp.lastLine}';
+      }
+    } else {
+      throw 'Invalid item count $exp on line ${exp.lastLine}';
+    }
+    if (name == 'create') {
+      final args = exp.args;
+      final int count;
+      if (args.length == 1) {
+        count = 1;
+      } else if (args.length == 2) {
+        count = (args[1] as IntegerExp).val;
+      } else {
+        throw 'Invalid item count $exp on line ${exp.lastLine}';
+      }
+      final itemName = (args[0] as StringExp).str;
+      return (Item(itemName), count);
+    } else if (name == '__TS__New') {
+      final args = exp.args;
+      if (args.length != 3) {
+        throw 'Invalid item count $exp on line ${exp.lastLine}';
+      }
+      final itemExp = (args[1] as TableAccessExp);
+      final prefix = (itemExp.prefixExp as NameExp?)?.name;
+      final key = (itemExp.keyExp as StringExp).str;
+      final itemName = '$prefix.$key';
+      final count = (args[2] as IntegerExp).val;
+      return (Item(itemName), count);
+    } else {
+      print(name);
+      throw 'Unsupported item count $name $exp on line ${exp.lastLine}';
+    }
+  }
+
+  Map<Item, int> _parseItemCounts(Exp exp) {
+    if (exp is! TableConstructorExp) {
+      throw 'Invalid item counts $exp on line ${exp.lastLine}';
+    }
     final items = <Item, int>{};
-    // This will be a list of Item.create("material.mana", 5)
-    final regexp = RegExp(r'Item.create\("(.*?)", (\d+)\)');
-    final matches = regexp.allMatches(lua);
-    for (final match in matches) {
-      final name = match.group(1)!;
-      final count = int.parse(match.group(2)!);
-      items[Item(name)] = count;
+    for (final valExp in exp.valExps) {
+      final (item, count) = _parseItemCount(valExp);
+      items[item] = count;
     }
     return items;
   }
 
-  Duration durationFromLua(String lua) {
-    final seconds = double.parse(lua.substring(8, lua.length - 1));
-    return Duration(microseconds: (seconds * 1000).floor());
+  CraftTab _parseCraftTab(Exp exp) {
+    if (exp is! TableAccessExp) {
+      throw 'Invalid CraftTab $exp on line ${exp.lastLine}';
+    }
+    final prefix = (exp.prefixExp as NameExp?)?.name;
+    if (prefix != 'CraftTab') {
+      throw 'Invalid CraftTab $exp on line ${exp.lastLine}';
+    }
+    final key = (exp.keyExp as StringExp).str;
+    return CraftTab.fromString("$prefix.$key");
   }
 
-  Set<CraftSite> sitesFromLua(String lua) {
-    final regexp = RegExp(r'(CraftSite\..*?),');
-    final matches = regexp.allMatches(lua);
-    return matches.map((m) => m.group(1)!).map(CraftSite.fromString).toSet();
-  }
-
-  Recipe recipeFromMatch(RegExpMatch match) {
-    // Get the line number from the match for errors.
-    final lineNumber = match.input.substring(0, match.start).split('\n').length;
-    print('Parsing recipe at line $lineNumber');
-
-    final name = match.namedGroup('name')!;
-    final inputs = match.namedGroup('inputs');
-    final outputs = match.namedGroup('outputs');
-    final time = match.namedGroup('time');
-    final sites = match.namedGroup('sites');
-    final tab = match.namedGroup('tab');
-    final id = match.namedGroup('id');
-    final unlocked = match.namedGroup('unlocked');
+  Recipe _parseCraftManagerAdd(FuncCallStat stat) {
+    // CraftManager:add(__TS__New(
+    //     CraftRecipe,
+    //     "IronIngot",
+    //     {Item.create("material.iron_ore", 1)},
+    //     {Item.create("material.iron_ingot", 1)},
+    //     seconds(6),
+    //     CraftSite.Forge,
+    //     CraftTab.Materials,
+    //     0,
+    //     true
+    // ))
+    final args = stat.exp.args;
+    if (args.length != 1) {
+      throw 'Invalid CraftManager:add call.';
+    }
+    if (args.first is! FuncCallExp) {
+      throw 'Invalid CraftManager:add call.';
+    }
+    final inner = args.first as FuncCallExp;
+    if (inner.args.length != 9) {
+      throw 'Invalid CraftRecipe call.';
+    }
+    // Ignore the first argument.
+    final name = (inner.args[1] as StringExp).str;
+    final inputs = _parseItemCounts(inner.args[2]);
+    final outputs = _parseItemCounts(inner.args[3]);
+    final time = _parseDuration(inner.args[4]);
+    final sites = _parseCraftSites(inner.args[5]);
+    final tab = _parseCraftTab(inner.args[6]);
+    final id = (inner.args[7] as IntegerExp).val.toInt();
+    final unlocked = (inner.args[8] is TrueExp);
     return Recipe(
-      name,
-      itemCountsFromLua(inputs!),
-      itemCountsFromLua(outputs!),
-      durationFromLua(time!),
-      sitesFromLua(sites!),
-      CraftTab.fromString(tab!),
-      int.parse(id!),
-      unlocked == 'true',
+      name: name,
+      inputs: inputs,
+      outputs: outputs,
+      duration: time,
+      sites: sites,
+      tab: tab,
+      id: id,
+      unlocked: unlocked,
     );
+  }
+
+  List<Recipe> parse(String content) {
+    final recipes = <Recipe>[];
+    final block = Parser.parse(content, 'main');
+    final addRecipes = block.stats.firstWhere((stat) {
+      if (stat is LocalFuncDefStat && stat.name == 'addRecipes') {
+        return true;
+      }
+      return false;
+    }) as LocalFuncDefStat;
+    for (final stat in addRecipes.exp.block.stats) {
+      if (stat is FuncCallStat) {
+        final name = stat.exp.nameExp?.str;
+        // We want the CraftManager:add calls.
+        if (name == "add") {
+          final Recipe recipe = _parseCraftManagerAdd(stat);
+          recipes.add(recipe);
+        }
+      }
+    }
+    return recipes;
   }
 }
 
@@ -151,37 +297,10 @@ void main(List<String> args) {
   }
 
   print('Reading recipes file: ${recipesFile.path}');
-
-  // Look for sections like:
-// CraftManager:add(__TS__New(
-//     CraftRecipe,
-//     "IronIngot",
-//     {Item.create("material.iron_ore", 1)},
-//     {Item.create("material.iron_ingot", 1)},
-//     seconds(6),
-//     CraftSite.Forge,
-//     CraftTab.Materials,
-//     0,
-//     true
-// ))
-  final regexp = RegExp(
-      r'CraftManager:add\(__TS__New\('
-      r'\s*CraftRecipe,$'
-      r'\s*"(?<name>.*)",$'
-      r'\s*\{(?<inputs>.*?)\},$'
-      r'\s*\{(?<outputs>.*?)\},$'
-      r'\s*(?<time>.*),$'
-      r'\s*(?<sites>.*),$'
-      r'\s*(?<tab>.*),$'
-      r'\s*(?<id>\d+),$'
-      r'\s*(?<unlocked>true|false)$'
-      r'\s*\)\)',
-      multiLine: true);
   final recipesLua = recipesFile.readAsStringSync();
-  final matches = regexp.allMatches(recipesLua);
-  print('Found ${matches.length} recipes.');
+
   final parser = RecipeParser();
-  final recipes = matches.map(parser.recipeFromMatch).toList();
+  final recipes = parser.parse(recipesLua);
   print('Parsed ${recipes.length} recipes.');
   // Generate the yaml files with the data.
 }
